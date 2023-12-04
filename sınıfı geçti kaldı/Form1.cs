0cce9a6a522f0510f7d033cdc95b24e75c9a79b0
ispir4747 <sehmusispir11@gmail.com>
2023-12-04 18:02:33 +0300

    Proje dosyası ekle.

---
+++ sınıfı geçti kaldı/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sınıfı_geçti_kaldı
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int viz = Convert.ToInt32(textBox1.Text);
+            int fin = Convert.ToInt32(textBox2.Text);
+            int yuz = (viz * 40) + (fin * 60) / 200;
+            if (yuz>=50)
+            { label3.Text = ("sınıfı_geçt"); }
+            else
+            { label3.Text = ("kaldınız"); }
+
+        }
+    }
+}
